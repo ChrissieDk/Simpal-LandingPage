2c3b2f88c8b76ae1e6735764cbd1a2cf0ccea39a
Christiaan de Kock <christiaandk96@gmail.com>
2022-07-15 12:34:17 +0200

    First commit, mailchimp setup

---
+++ src/components/Signup.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {MailChimpForm} from './MailChimpForm';
+
+function Contact() {
+  return (
+    
+    <div className='container'>
+    
+      <h1>Coming Soon</h1>
+      <p>We are working hard to bring you a better experience</p>
+    
+      <MailChimpForm />
+      
+      
+    </div>
+    
+    
+  );
+}
+export default Contact;
